528e941b791a98390c61ce878719d1c49dcec88b
Dean <dsdy120@gmail.com>
2024-11-23 23:19:00 -0500

    .

--- reference_frame.py
+++ reference_frame.py
@@ -32,7 +32,7 @@ def observation_site_ECI_vector(latitude_deg,datum_elevation_km,loc_sidereal_deg
 
     return np.array(
         [I_component, J_component, K_component]
-    )
+    ).T # Transpose to column vector
 
 def target_track_ENZ_vector(azimuth_deg,elevation_deg,range_km):
     '''
@@ -51,6 +51,43 @@ def target_track_ENZ_vector(azimuth_deg,elevation_deg,range_km):
 
     return np.array(
         [I_component, J_component, K_component]
-    )
+    ).T # Transpose to column vector
 
 def ECI_to_ENZ_matrix(latitude_deg,loc_sidereal_deg):
+    '''
+    Returns transformation matrix from ECI to ENZ frame.
+    '''
+    if latitude_deg < -90.0 or latitude_deg > 90.0:
+        raise ValueError("Latitude out of range")
+    
+    loc_sidereal_deg = loc_sidereal_deg % 360.0
+    
+    latitude_rad = np.radians(latitude_deg)
+    loc_sidereal_rad = np.radians(loc_sidereal_deg)
+
+    r11 = -np.sin(loc_sidereal_rad)
+    r12 = np.cos(loc_sidereal_rad)
+    r13 = 0.0
+
+    r21 = -np.sin(latitude_rad) * np.cos(loc_sidereal_rad)
+    r22 = -np.sin(latitude_rad) * np.sin(loc_sidereal_rad)
+    r23 = np.cos(latitude_rad)
+
+    r31 = np.cos(latitude_rad) * np.cos(loc_sidereal_rad)
+    r32 = np.cos(latitude_rad) * np.sin(loc_sidereal_rad)
+    r33 = np.sin(latitude_rad)
+
+
+    return np.array(
+        [
+            [r11, r12, r13],
+            [r21, r22, r23],
+            [r31, r32, r33]
+        ]
+    )
+
+def ENZ_to_ECI_matrix(latitude_deg,loc_sidereal_deg):
+    '''
+    Returns transformation matrix from ENZ to ECI frame.
+    '''
+    return ECI_to_ENZ_matrix(latitude_deg,loc_sidereal_deg).T
